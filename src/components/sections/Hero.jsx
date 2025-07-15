93e179abc348cb8bf4f28d29d13a717e63ace96b
anjaliGhoghari <anjalighoghari79@gmail.com>
2025-07-15 17:14:39 +0530

    fix:hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -25,7 +25,7 @@ function Hero() {
   />
   <div className="relative z-20 mt-[88px] mx-[88px] flex items-start justify-start flex-col h-full">
     <h1 className={`${styles.heading1} max-w-[700px] mb-3`}>Yields from the world’s top asset managers made accessible.</h1>
-    <p className={`${styles.paragraph} text-[#F7F9FC] max-w-[360px] mb-14`}>The smart gateway to institutional-grade, delta-neutral crypto yields.</p>
+    <p className={`${styles.paragraph} !text-[#F7F9FC] max-w-[360px] mb-14`}>The smart gateway to institutional-grade, delta-neutral crypto yields.</p>
     <Button variant="white"/>
     <Partners/>
   </div>
